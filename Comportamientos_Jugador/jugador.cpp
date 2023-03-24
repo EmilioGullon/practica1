33d708bec34b496d5e38760eee6d8326a423d013
egullonl01@corre.ugr.es <egullonl01@gmail.com>
2023-03-24 16:48:44 +0100

    Actualización Tutorial

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -1,12 +1,13 @@
 #include "../Comportamientos_Jugador/jugador.hpp"
 #include <iostream>
+#include <cmath> 
 using namespace std;
 
 
 
 Action ComportamientoJugador::think(Sensores sensores){
 
-	Action accion = actIDLE;
+	Action accion=actFORWARD;
 
 	cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC << " ";
 	switch(sensores.sentido){
@@ -34,11 +35,229 @@ Action ComportamientoJugador::think(Sensores sensores){
 	cout << "Vida: " << sensores.vida << endl;
 	cout << endl;
 
-
+	int a;
 	// Determinar el efecto de la ultima accion enviada
+	switch (last_action){
+	case actFORWARD:
+	switch (current_state.brujula){
+	case norte: current_state.fil--; break;
+	case noreste: current_state.fil--; current_state.col++; break;
+	case este: current_state.col++; break;
+	case sureste: current_state.fil++;current_state.col++; break;
+	case sur: current_state.fil++; break;
+	case suroeste: current_state.fil++;current_state.col--; break;
+	case oeste: current_state.col--; break;
+	case noroeste: current_state.fil--;current_state.col--; break;
+	}	
+	break;
+	case actTURN_SL:
+		a = current_state.brujula;
+		a = (a+7)%8;
+		current_state.brujula = static_cast<Orientacion>(a);
+	break;
+	case actTURN_SR:
+		a = current_state.brujula;
+		a = (a+1)%8;
+		current_state.brujula = static_cast<Orientacion>(a);
+	break;
+	case actTURN_BL:
+	// Actualización de girar 135º a la izquierda
+		a = current_state.brujula;
+		a = (a+5)%8;
+		current_state.brujula = static_cast<Orientacion>(a);
+	break;
+	case actTURN_BR:
+	// Actualización en caso de girar 135º a la derecha0
+		a = current_state.brujula;
+		a = (a+3)%8;
+		current_state.brujula = static_cast<Orientacion>(a);
+	break;
+	}
+
+
+	if (sensores.terreno[0]=='G' and !bien_situado){
+		current_state.fil = sensores.posF;
+		current_state.col= sensores.posC;
+		current_state.brujula = sensores.sentido;
+		bien_situado = true;
+	}
+
+	if (bien_situado){
+		PonerTerrenoEnMatriz(sensores.terreno,current_state,mapaResultado);
+	}
+
+	if ((sensores.terreno[2]=='T' or sensores.terreno[2]=='S' or sensores.terreno[2]=='G')  and  sensores.superficie[2]=='_'){
+	accion = actFORWARD;
+	} else if (!girar_derecha){
+	accion = actTURN_SL;
+	girar_derecha = (rand()%2 ==0);
+	} else{
+	accion = actTURN_SR;
+	girar_derecha = (rand()%2 ==0);
+	}
+
+	//todo
+	last_action = accion;
+
 	return accion;
 }
 
 int ComportamientoJugador::interact(Action accion, int valor){
   return false;
 }
+
+
+
+
+
+
+
+
+void PonerTerrenoEnMatriz(const vector<unsigned char> &terreno, const state &st, vector< vector<unsigned char> > &matriz){
+	int fil, col;
+	int cont=1;
+	int fil1=0, col1=0;
+	matriz[st.fil][st.col]  = terreno [0];
+	switch (st.brujula){
+	case norte: 
+		fil=-1;
+		cont=1;
+		fil1=0, col1=0;
+		for(int i=1;i<4;i++){
+			fil1=i;
+			for (col1 = -i; col1 <= i; col1++){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+col1]  = terreno [cont];
+				cont++;
+			}
+		}
+	break;
+	case noreste: 
+		fil=-1;
+		col=1;
+		cont=1;
+		fil1=0, col1=0;
+		for(int i=1;i<4;i++){
+			fil1=i;
+			for (col1 = 0; col1 < i; col1++){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+			
+			for (fil1; fil1 >= 0; fil1--){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+		}		
+	break;
+	case este:  
+		col=1;
+		cont=1;
+		fil1=0, col1=0;
+		for(int i=1;i<4;i++){
+			col1=i;
+			for (fil1 = -i; fil1 <= i; fil1++){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+		}
+	break;
+	case sureste: 
+		fil=1;
+		col=1;
+		cont=1;
+		fil1=0, col1=0;
+		for(int i=1;i<4;i++){
+			col1=i;
+			for (fil1 = 0; fil1 < i; fil1++){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+			
+			for (col1; col1 >= 0; col1--){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+		}		
+	break;
+	case sur:  
+		fil=1;
+		cont=1;
+		fil1=0, col1=0;
+		for(int i=1;i<4;i++){
+			fil1=i;
+			for (col1 = -i; col1 <= i; col1++){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+-col1]  = terreno [cont];
+				cont++;
+			}
+		}
+	break;
+	case suroeste: 
+		fil=1;
+		col=-1;
+		 cont=1;
+		fil1=0, col1=0;
+		for(int i=1;i<4;i++){
+			fil1=i;
+			for (col1 = 0; col1 < i; col1++){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+			
+			for (fil1; fil1 >= 0; fil1--){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+		}		
+	break;
+	case oeste: 
+		col=-1;
+		cont=1;
+		fil1=0, col1=0;
+		for(int i=1;i<4;i++){
+			col1=i;
+			for (fil1 = -i; fil1 <= i; fil1++){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+-fil1][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+		}
+	break;
+	case noroeste:  
+		fil=-1;
+		col=-1;
+		cont=1;
+		fil1=0, col1=0;
+		for(int i=1;i<4;i++){
+			col1=i;
+			for (fil1 = 0; fil1 < i; fil1++){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+			
+			for (col1; col1 >= 0; col1--){
+				cout<<fil1<<" and "<<col1<<" = "<< cont <<endl;
+				matriz[st.fil+fil1*fil][st.col+col1*col]  = terreno [cont];
+				cont++;
+			}
+		}		
+	break;
+	}	
+
+	
+	
+	
+
+
+	
+}
+
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -4,12 +4,23 @@
 #include "comportamientos/comportamiento.hpp"
 using namespace std;
 
-class ComportamientoJugador : public Comportamiento{
+struct state{
+  int fil;
+  int col;
+  Orientacion brujula;
+};
 
+
+class ComportamientoJugador : public Comportamiento{
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
       // Dar el valor inicial a las variables de estado
+      current_state.fil=current_state.col=99;
+      last_action = actIDLE;
+      current_state.brujula = norte;
+      girar_derecha=false;
+      bien_situado=false;
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -17,11 +28,32 @@ class ComportamientoJugador : public Comportamiento{
 
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
+    
 
   private:
-  
+  /*
   // Declarar aquí las variables de estado
+  int vida;
+  int bateria;
+  bool colision;
+  bool reset;
+  int posF;
+  int posC;
+  Orientacion sentido; // Orientacion
+  double tiempo;
+  vector< unsigned char> terreno;
+  vector< unsigned char> superficie;
+  */
+
+  state current_state;
+  Action last_action;
+	bool girar_derecha,
+  bien_situado;
+
 
 };
+
+void PonerTerrenoEnMatriz(const vector<unsigned char> &terreno, const state &st, vector< vector<unsigned char> > &matriz);
+
 
 #endif
